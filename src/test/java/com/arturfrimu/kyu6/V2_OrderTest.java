fc760c5ee96bcae4a9492f7b6c27bb7a313cbe52
Artur <artur.frimu2@gmail.com>
2024-11-05 17:54:45 +0200

    V2_OrderTest V3

--- src/test/java/com/arturfrimu/kyu6/V2_OrderTest.java
+++ src/test/java/com/arturfrimu/kyu6/V2_OrderTest.java
@@ -3,9 +3,9 @@ package com.arturfrimu.kyu6;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeMap;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,70 +14,58 @@ import static org.junit.Assert.assertEquals;
  * Each word in the input contains a number indicating its position in the sorted output.
  */
 public class V2_OrderTest {
-    public static String order(String words) {
+
+    public static String orderV2(String words) {
         if (words.isBlank()) {
             return "";
         }
 
-        String[] splitedWords = words.split(" ");
+        return Optional.ofNullable(Arrays.stream(words.split(" "))
+                .map(splitedWord -> {
+                    int wordPosition = splitedWord.toLowerCase().chars().boxed()
+                            .filter(Character::isDigit)
+                            .distinct()
+                            .findFirst()
+                            .map(Character::getNumericValue)
+                            .orElseThrow(() -> new RuntimeException("This word not contains a position : " + splitedWord));
+                    return new PositionToWord(wordPosition, splitedWord);
+                }).sorted(Comparator.comparing(PositionToWord::position))
+                .map(el -> el.word)
+                .collect(Collectors.joining(" "))).orElse("");
+    }
 
-        Map<Integer, Stack<String>> linkedHashMap = new TreeMap<>();
+    public static String orderV1(String words) {
+        return Arrays.stream(words.split(" "))
+                .sorted(Comparator.comparing(s -> s.replaceAll("\\D+", "")))
+                .collect(Collectors.joining(" "));
+    }
 
-        Arrays.stream(splitedWords)
-                .forEach(splitedWord -> {
-                    int wordPosition = splitedWord.toLowerCase()
-                                               .chars()
-                                               .boxed()
-                                               .filter(Character::isDigit)
-                                               .distinct()
-                                               .findFirst()
-                                               .map(Character::getNumericValue)
-                                               .get();
-
-                    if (linkedHashMap.containsKey(wordPosition)) {
-                        linkedHashMap.get(wordPosition).push(splitedWord);
-                    } else {
-                        Stack<String> objects = new Stack<>();
-                        objects.push(splitedWord);
-                        linkedHashMap.put(wordPosition, objects);
-                    }
-                });
-
-        String result = "";
-        for (Map.Entry<Integer, Stack<String>> entry : linkedHashMap.entrySet()) {
-            Stack<String> stack = entry.getValue();
-            int size = stack.size();
-            for (int i = 0; i < size; i++) {
-                result = result + " " + stack.pop();
-            }
-        }
-
-        return result.trim();
+    public record PositionToWord(int position, String word) {
     }
 
     @Test
     public void testBasicOrder() {
-        assertEquals("Thi1s is2 3a T4est", order("is2 Thi1s T4est 3a"));
+        assertEquals("Thi1s is2 3a T4est", orderV1("is2 Thi1s T4est 3a"));
     }
 
     @Test
     public void testMixedOrder() {
-        assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", order("4of Fo1r pe6ople g3ood th5e the2"));
+        assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", orderV1("4of Fo1r pe6ople g3ood th5e the2"));
     }
 
     @Test
     public void testEmptyInput() {
-        assertEquals("", order(""));
+        assertEquals("", orderV1(""));
     }
 
     @Test
     public void testSingleWord() {
-        assertEquals("wor1d", order("wor1d"));
+        assertEquals("wor1d", orderV1("wor1d"));
     }
 
     @Test
     public void testDuplicateNumbers() {
-        assertEquals("wo1rd wor1d", order("wor1d wo1rd"));
+        assertEquals("wo1rd wor1d", orderV1("wo1rd wor1d"));
     }
 }
 
