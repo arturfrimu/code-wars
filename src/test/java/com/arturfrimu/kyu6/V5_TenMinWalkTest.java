8ce03aa8ea984d8b07328792c0a0f75c0a9bda47
Artur <artur.frimu2@gmail.com>
2024-11-06 17:10:11 +0200

    V5_TenMinWalkTest

--- src/test/java/com/arturfrimu/kyu6/V5_TenMinWalkTest.java
+++ src/test/java/com/arturfrimu/kyu6/V5_TenMinWalkTest.java
@@ -1,7 +1,13 @@
 package com.arturfrimu.kyu6;
 
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Optional.ofNullable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -10,10 +16,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class V5_TenMinWalkTest {
 
-    // Method to test the walk's validity (Placeholder for logic implementation)
     public static boolean isValid(char[] walk) {
-        // TODO implement your logic here
-        return true;
+        if (walk.length != 10) return false;
+
+        Map<Character, List<Character>> result = new String(walk)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c));
+
+        var n = size(result.get('n'));
+        var s = size(result.get('s'));
+        var e = size(result.get('e'));
+        var w = size(result.get('w'));
+        return n.equals(s) && e.equals(w);
+    }
+
+    private static Integer size(List<Character> value) {
+        return ofNullable(value).map(List::size).orElse(0);
     }
 
     private static void doTest(char[] input, boolean expected) {
@@ -22,34 +41,39 @@ public class V5_TenMinWalkTest {
         assertEquals(expected, actual, message);
     }
 
+//    @Test
+//    void falsea() {
+//        doTest(new char[]{'n', 's', 'e', 'w'}, false);
+//    }
+
     @Test
     void falseWalkTooShort() {
-        doTest(new char[] {'n'}, false);
-        doTest(new char[] {'n', 's'}, false);
+        doTest(new char[]{'n'}, false);
+        doTest(new char[]{'n', 's'}, false);
     }
 
     @Test
     void falseWalkTooLong() {
-        doTest(new char[] {'n','s','n','s','n','s','n','s','n','s','n','s'}, false);
-        doTest(new char[] {'n','s','e','w','n','s','e','w','n','s','e','w','n','s','e','w'}, false);
-        doTest(new char[] {'n','s','e','w','n','s','e','w','n','s','e','w','n','s','e','w','n','s','e','w'}, false);
+        doTest(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}, false);
+        doTest(new char[]{'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w'}, false);
+        doTest(new char[]{'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w'}, false);
     }
 
     @Test
     void falseDoesNotLoopBackToStart() {
-        doTest(new char[] {'n','s','n','s','n','s','n','s','n','n'}, false);
-        doTest(new char[] {'e','e','e','w','n','s','n','s','e','w'}, false);
-        doTest(new char[] {'n','e','n','e','n','e','n','e','n','e'}, false);
-        doTest(new char[] {'n','w','n','w','n','w','n','w','n','w'}, false);
-        doTest(new char[] {'s','e','s','e','s','e','s','e','s','e'}, false);
-        doTest(new char[] {'s','w','s','w','s','w','s','w','s','w'}, false);
+        doTest(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 'n'}, false);
+        doTest(new char[]{'e', 'e', 'e', 'w', 'n', 's', 'n', 's', 'e', 'w'}, false);
+        doTest(new char[]{'n', 'e', 'n', 'e', 'n', 'e', 'n', 'e', 'n', 'e'}, false);
+        doTest(new char[]{'n', 'w', 'n', 'w', 'n', 'w', 'n', 'w', 'n', 'w'}, false);
+        doTest(new char[]{'s', 'e', 's', 'e', 's', 'e', 's', 'e', 's', 'e'}, false);
+        doTest(new char[]{'s', 'w', 's', 'w', 's', 'w', 's', 'w', 's', 'w'}, false);
     }
 
     @Test
     void trueIsValid10MinutesWalk() {
-        doTest(new char[] {'n','s','n','s','n','s','n','s','n','s'}, true);
-        doTest(new char[] {'e','w','e','w','n','s','n','s','e','w'}, true);
-        doTest(new char[] {'n','s','e','w','n','s','e','w','n','s'}, true);
+        doTest(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}, true);
+        doTest(new char[]{'e', 'w', 'e', 'w', 'n', 's', 'n', 's', 'e', 'w'}, true);
+        doTest(new char[]{'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's'}, true);
     }
 }
 
